2fcc2eb2db5b47c12268060b7d4593b062c460b4
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-10-03 11:45:31 +0200

    asdf

--- src/screens/auth/AppLogout.jsx
+++ src/screens/auth/AppLogout.jsx
@@ -5,7 +5,7 @@ import authService from "./../../utils/services/auth-service"
 const AppLogout = props => {
   useEffect(() => {
     authService.logout()
-    props.history.push("/")
+    props.history.push("/auth/login")
   }, [props.history])
 
   return null
